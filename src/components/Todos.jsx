bb85730e9977035adaed49d81690fb16512b7bf7
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-03-04 15:32:29 +0400

    logic creating task

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
 
-const Todos = () => {
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    addTodo: (obj) => dispatch(addTodos(obj))
+  }
+}
+
+
+const Todos = (props) => {
   const [todo, setTodo] = useState('');
 
   const handleChange = (e) => {
     setTodo(e.target.value)
   }
 
-  console.log('todo text', todo);
+  console.log('props from store', props);
 
   return (
     <div className="addTodos">
@@ -15,9 +30,16 @@ const Todos = () => {
        onChange={(e) => handleChange(e)}
        className="todo-input"
         />
-      <button className="add-btn">Add</button>
+      <button className="add-btn" 
+      onClick={() => props.addTodo({
+        id: Math.random().toString(16).slice(2),
+        item: todo,
+        completed: false
+      })}
+      >Add</button>
     </div>
   )
 }
 
-export default Todos;
+// connect связывает Counter с Redux Store, передавая состояние и действия в качестве свойств
+export default connect(mapStateToProps,mapDispatchToProps)(Todos);
